69e1c3872ff2be3ebd5150b51c1bca8ceaa6b7c0
János Benjamin Antal <antal.janos.benjamin@gmail.com>
2019-07-21 15:00:10 +0200

    Add minimal documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,19 @@
 //! Simple stopwatch implementation that can be used for high resolution time measurement.
-//! 
-//! # Examples
-//! ```
+//!
+//! # Example
+//!
+//! ```no_run
+//! use hrsw::Stopwatch;
+//!
 //! let mut stopwatch = Stopwatch::new();
-//! stopwatch.start()
-//! ...
-//! stopwatch.
+//! stopwatch.start();
+//! // do something
+//! let elapsed = stopwatch.elapsed();
+//! // do something other
+//! stopwatch.stop();
+//! let total_elapsed = stopwatch.elapsed();
 //! ```
 use std::time::Duration;
-use time;
 
 pub struct Stopwatch {
     start_time: Option<u64>,
@@ -16,7 +21,6 @@ pub struct Stopwatch {
 }
 
 impl Stopwatch {
-
     /// Creates a Stopwatch.
     pub fn new() -> Stopwatch {
         Stopwatch {
@@ -36,36 +40,45 @@ impl Stopwatch {
     }
 
     /// Starts the measurement.
+    /// If the stopwatch is already running, then the call has no effect.
     pub fn start(&mut self) {
         if self.start_time.is_none() {
             self.start_time = Some(time::precise_time_ns());
         }
     }
 
-    /// Stops the measurement. The elapsed duration can be obtained using 'elapsed()'.
+    /// Stops the measurement.
+    /// The elapsed duration can be obtained using `elapsed()`. If the stopwatch has never been started or has already been stoped, then the call has no effect.
     pub fn stop(&mut self) {
         if self.start_time.is_some() {
             self.elapsed_time =
                 self.elapsed_time + (time::precise_time_ns() - self.start_time.take().unwrap());
         }
     }
-    
+
+    /// Restores the original state of the stopwatch.
+    /// If the stopwatch is running, then it will be stoped and the elapsed will be cleared, so it can't be obtained.
     pub fn reset(&mut self) {
         self.start_time = None;
         self.elapsed_time = 0;
     }
 
+    /// Restores the original state of the stopwatch and then starts the measurement.
+    /// It is the same as calling `reset()` and `start()` in that sequence.
     pub fn reset_and_start(&mut self) {
         self.reset();
         self.start();
     }
 
+    /// Returns the elapsed time. In case of multiple `start()` and `stop()` the elapsed intervals are accumulated. The elapsed time can be cleared by `reset()` or reset_and_start()`.
     pub fn elapsed(&self) -> Duration {
         match self.start_time {
             Some(t) => Duration::from_nanos(self.elapsed_time + (time::precise_time_ns() - t)),
             None => Duration::from_nanos(self.elapsed_time),
         }
     }
+
+    /// Returns whether the stopwatch is running or not.
     pub fn is_running(&self) -> bool {
         self.start_time.is_some()
     }
@@ -125,7 +138,6 @@ mod tests {
         thread::sleep(DURATION_TO_USE);
         assert_eq_with_min(&stopwatch, 3 * DURATION_TO_USE);
     }
-    
     #[test]
     fn get_elapsed_multiple_times() {
         let mut stopwatch = Stopwatch::new();
